ed26ca545f927797d6083dc3de489261b32a3be9
AgusEma <agusallocco12@gmail.com>
2024-01-23 12:52:26 -0300

    primer commit

---
+++ ejemplos-ado-net/frmPokemons.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ejemplos_ado_net
+{
+    partial class frmPokemons
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPokemons = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemons)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPokemons
+            // 
+            this.dgvPokemons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPokemons.Location = new System.Drawing.Point(12, 12);
+            this.dgvPokemons.Name = "dgvPokemons";
+            this.dgvPokemons.Size = new System.Drawing.Size(426, 218);
+            this.dgvPokemons.TabIndex = 0;
+            // 
+            // frmPokemons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 322);
+            this.Controls.Add(this.dgvPokemons);
+            this.Name = "frmPokemons";
+            this.Text = "frmPokemons";
+            this.Load += new System.EventHandler(this.frmPokemons_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemons)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPokemons;
+    }
+}
+
---
+++ ejemplos-ado-net/frmPokemons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplos_ado_net
+{
+    public partial class frmPokemons : Form
+    {
+        public frmPokemons()
+        {
+            InitializeComponent();
+        }
+
+        //Invoco la lectura (ya construida) a base de datos:
+        private void frmPokemons_Load(object sender, EventArgs e)
+        {
+                
+            PokemonNegocio negocio = new PokemonNegocio();
+            //DataSource recibe un origen de datos y lo modela en la tabla:
+            dgvPokemons.DataSource = negocio.listar();
+        }
+    }
+}
